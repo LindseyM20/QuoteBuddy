d0819787a9050882ffa199fa587a86516174cdd2
Jishllg Laptop <jishllg@gmail.com>
2020-10-07 12:00:22 -0500

    Added basic local storage functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,18 @@ function displayQuote(quoteData) {
     console.log(quoteData.quote.quoteGenre);
     console.log(quoteData.quote.quoteText);
 }
+
+// Getting previously saved quotes from local storage and creating a new array if one does not exist
+var savedQuotes = loadSaved();
+function loadSaved() {
+  var saved = JSON.parse(localStorage.getItem("saved"));
+  if (saved === null) {
+      saved = ["This is a test quote. -Josh"];
+  }
+  return saved;
+}
+// Saving a quote to the array of saved quotes and storing locally
+function saveQuote(quote) {
+  savedQuotes.unshift(quote);
+  localStorage.setItem("saved", JSON.stringify(savedQuotes));
+}
