6189e5122cbf3815b9c0f98ee83935e4b84fdf7c
LindseyM20 <lindsey.bordner@gmail.com>
2020-10-08 14:07:54 -0500

    redid some overall button styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -58,7 +58,7 @@ function renderSaved() {
   quoteHistory.empty()
   savedQuotes.forEach(function(q, i) {
     quoteHistory.append($("<div>" + 
-      '<button id="display' + i + '" class="displayBtn pure-buttonpure-button-primary pure-input-1">' 
+      '<button id="display' + i + '" class="displayBtn pure-button pure-button-primary pure-input-1">' 
       + q.substring(0, 15) + '...</button>' + 
       '<button id="del' + i + '" class="delBtn pure-button pure-button-primary pure-input-1">' + 
       '<i class="fas fa-times"></i></button></div>'))
